0354987dd6afa33ff925468be6a36b31dda46bda
LouisLe999 <khoe.le.mall@gmail.com>
2023-11-08 17:19:35 +0700

    #imp: test-sendEmail

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import CardBox from 'components/card'
 import './App.css'
+import CardBox from './components/card'
+import { Button } from './components/ui/button'
 
 // https://jsonplaceholder.typicode.com/todos
 function App() {
+
+    const body = {
+      
+        "emailTo": "ngockhoe.le@kepland.com.vn",
+        "token": "YnKePgTZVn9ohthTwAfQxQL0Fu1zWnQfKbJmFJGTV7HW73XQ9FUA",
+        "requestedBy": "Khoe Le",
+        "emailCC": "ngockhoe.le@kepland.com.vn",
+        "approvalPerson": "A. Thien",
+        "requestCode": "azkxfk3k2k32k",
+        "requestDescription": "ke ba tao diii",
+        "categoryName": "ví duu",
+        "requestDisplayName": "ai biet",
+        "linkOfRequest": "https://portal.keppland.com.vn",
+        "type": 2
+    }
+
+    const url = 'https://klvportalapidev.azurewebsites.net/api/Common/EASSendEmail'
+
+    const handleSendEmail  = async () => {
+        const res = await fetch(url, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify(body)
+        })
+
+        console.log(res)
+
+        const data = await res.json()
+
+        console.log(data)
+    }
 
   return (
     <div className='py-12 px-12 relative'>
@@ -13,6 +47,10 @@ function App() {
         <div className=' flex justify-center mb-4 py-6'>
           <img src='https://github.com/pmndrs/zustand/raw/main/bear.jpg' alt='' />
         </div>
+      </div>
+
+      <div>
+        <Button onClick={handleSendEmail}>Send Email</Button>
       </div>
 
       <div className='px-12 py-12 flex flex-1 justify-center content-center'>
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      // find: '@', replacement: path.resolve(__dirname, "./src")
       "@": path.resolve(__dirname, "./src"),
     },
   },
